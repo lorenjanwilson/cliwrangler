bb5d113659a8d261649f58c9f91f50d2243f00b3
loren jan wilson <loren_wilson@harvard.edu>
2015-03-24 18:03:40 -0400

    Major improvements to output and prompt detection code, as well as identification code

--- cliwrangler.py
+++ cliwrangler.py
@@ -49,9 +49,11 @@ class CLIWrangler:
         # The last prompt we found, and a boolean for whether or not the prompt changed after the last command send.
         self.prompt = None
         self.prompt_changed = False
+        # The prefix of the prompt that we try to consistently match. Let's see if this works.
+        self.prompt_prefix = None
         # A list of keywords used to identify this device. Discovered via things like "show ver".
         # This might be something like ['Cisco', 'IOS', 'C3750'] or ['Cisco', 'NX-OS', 'Nexus', '5000']
-        self.identification = []
+        self.identifiers = []
 
         # Initialize Paramiko and Paramiko-expect objects.
         self.client = paramiko.SSHClient()
@@ -90,8 +92,12 @@ class CLIWrangler:
         # Now we can initialize our interaction object.
         self.interact = paramikoe.SSHClientInteraction(self.client, timeout=self.timeout, display=self.echo)
 
-        # Get the prompt.
-        self._get_prompt()
+        # Hit a carriage return to make sure we can sense the prompt.
+        # This isn't necessary on Cisco IOS, but it's necessary on FortiOS.
+        self.interact.send(self.newline)
+
+        # Expect before continuing, to clear the buffer and set the prompt.
+        self._expect_output()
 
         # Prep the session (term len 0, etc).
         self._prepare()
@@ -101,98 +107,161 @@ class CLIWrangler:
 
         return True
 
-    def _get_prompt(self):
-        """Figure out the device prompt. We will probably need to do this
-        after running each command, just to make sure the prompt hasn't
-        changed due to a mode change."""
+    def _expect_output(self):
+        """Expect our entire output. This is meant to be used during a "send"
+        method call, or after any manual send. There are some dirty tricks in here."""
 
+        # The first thing we do is type a string that we don't expect to get
+        # from any command output. This puts us in a known state. Otherwise, if
+        # we try to just match on the prompt, we won't know whether we're
+        # matching the last prompt available, or something before we ran the
+        # last command.
         unique_string = 'RRRR'
         length_of_unique_string = len(unique_string)
         backspace_string = self.backspace * length_of_unique_string
 
-        # First, send a newline.
-        self.interact.send(self.newline)
-        # Send a string that will signify the absolute end of the buffer.
-        # Sad, but necessary. If we don't do this, we won't know whether
-        # we're matching the very last thing we got.
+        # Send our unique string.
         self.interact.channel.send(unique_string)
-        self.interact.expect(".*%s$" % (unique_string))
-        # See what's now at the end.
-        output = self.interact.current_output
-        # Strip the extra stuff we created off the end.
-        output = output[:-length_of_unique_string]
-        # Take the last line. That's our prompt.
-        prompt = output.splitlines()[-1];
+        # Expect the unique string we just sent, with the prompt prefix at the
+        # beginning of the line if we have one.
+        if self.prompt_prefix:
+            self.interact.expect("%s.*%s" % (self.prompt_prefix, unique_string))
+        else:
+            # If we don't have a prompt prefix, at least expect something sane.
+            # Unfortunately, this is a bit more complex, because we have to
+            # define sanity. It should be "something kind of prompty" followed
+            # by an allowed end-of-prompt character followed by the unique
+            # string we typed.
+            end_of_prompt_chars = ['>', '#', '$', '%']
+            # A prompt, simply, is 3 or more [a-z0-9-] characters, plus maybe
+            # an extension of other things, with an end_of_prompt_char at the end,
+            # and maybe some whitespace after that.
+            general_prompt_regex_template = '[a-zA-Z0-9-]{3,}\s*%s\s*%s'
+            # Create a regex for each char in the list above.
+            general_prompt_regexes = [ general_prompt_regex_template % (char, unique_string) for char in end_of_prompt_chars ]
+            # See if any of the "sane-looking possible prompts" matched.
+            self.interact.expect(general_prompt_regexes)
+
+        # Set the output variables.
+        self.output = self.interact.current_output_clean
+        self.output_raw = self.interact.current_output
+
         # Backspace over the dirty trick.
         self.interact.channel.send(backspace_string)
 
-        # Check to see if the prompt has changed.
-        if (self.prompt == prompt):
-            self.prompt_changed = False
-        else:
-            self.prompt_changed = True
-            self.prompt = prompt
+        # We now need to set the "prompt" variable in case the prompt changed.
+        # See what's now at the end.
+        output = self.output_raw
+        # Strip the extra stuff we created off the end.
+        output = output[:-length_of_unique_string]
+        # Take the last line. That's our prompt.
+        self.prompt = output.splitlines()[-1];
 
-        return self.prompt
+        # Try to get the prompt prefix if we haven't already. We keep track of
+        # this in case the prompt gets suffixes tacked onto it by mode changes.
+        # If we don't do this, we'll trip up when a prompt-ending character is
+        # found at the end of a line of command output. 
+        if self.prompt_prefix is None:
+            m = re.match('^([a-zA-Z0-9-]{3,})', self.prompt)
+            if m is not None:
+                self.prompt_prefix = m.group(1)
+
+        # Let's return True. I have no idea what would be useful here yet.
+        return True
 
     def _prepare(self):
         """Prepare the session by doing "terminal length 0" and any other
         such things that might be necessary."""
 
-        # This is going to have to do much more in the future, but right now it just sends a "term len 0".
-        self.interact.send('terminal length 0')
-        self.interact.expect(self.prompt)
-        return True
+        # The very first thing we need to do is turn off paging.
+        result = self.send('terminal length 0', graceful=True)
+        # If that worked, send IOS commands to disable monitor and editing.
+        if result is not None:
+            self.send('terminal no monitor', graceful=True)
+            self.send('terminal no editing', graceful=True)
+            return True
+
+        # Cisco ASAs don't let you run "terminal pager 0" unless you've
+        # enabled. This is severely cramping my style. Not sure how to deal
+        # with this case yet.
+
+        # The usual IOS commands didn't work, so let's try the FortiOS ones.
+        # This is horribly dirty because FortiOS CLI is terrible.
+        # I don't feel comfortable trying these things unless we're relatively
+        # sure we're on FortiOS, so let's look for the Fortinet error format in
+        # the output of our last command.
+        if "Command fail. Return code" in self.output:
+            # There are two ways to do this. Here's the one you need if you have vdoms:
+            result = self.send('config global', graceful=True)
+            if result is not None:
+                self.send('config system console')
+                self.send('set output standard')
+                self.send('end')
+                self.send('end')
+            # And here's the one you need if you don't have vdom support (fortiOS 4.x?)
+            else:
+                self.send('config system console')
+                self.send('set output standard')
+                self.send('end')
+
+        # None of our prep worked. This may not be catastrophic, so let's return False.
+        return False
         
     def _identify(self):
         """Run various commands to identify the device we're on."""
 
-        # Here's a big collection of strings to look for.
-        # If we find any of these strings in command output, we add their value
-        # to the identification list.
+        # Here's a big collection of strings to look for. If we find any of
+        # these strings in command output, we add their value to the
+        # identification list. I should probably load this in from a separate
+        # file because it's going to get huge.
         identification_strings = yaml.load("""
             'Cisco ': 'Cisco'
             'cisco ': 'Cisco'
             'CISCO ': 'Cisco'
             ' IOS ': 'IOS'
             ' C3750 ': 'C3750'
+            'Adaptive Security Appliance': 'ASA'
+            'ASA5520': 'ASA5520'
+            'FWSM Firewall': 'FWSM'
+            'FWSM Firewall Version': 'Cisco'
+            'FortiOS': 'Fortinet'
+            'FortiGate': 'FortiGate'
+            'FortiGate-1000C': '1000C'
+            'Nexus Operating': 'Nexus'
+            'NX-OS': 'NX-OS'
+            'Nexus5548': 'Nexus5548'
         """)
 
-        # Run a show ver and hope for the best.
-        # In the future, we might run a slew of commands and search through the
-        # output of any of the ones that worked.
-        self.send('show version')
+        identification_commands = yaml.load("""
+            - 'show version' # Cisco IOS
+            - 'get system status' # Fortinet FortiOS
+        """)
+
+        # Run a slew of commands and search through the output of the one (or
+        # ones) that worked. Right now, we break after one successful command
+        # run.
+        output = None
+
+        for command in identification_commands:
+            # "graceful" just means "Don't raise an exception because this
+            # command might fail and I don't care if it does."
+            result = self.send(command, graceful=True)
+            if result is not None:
+                output = self.output
+                break
 
         # If any of the identification strings are found in the output, stick
-        # them into the identification array.
+        # them into the identifiers list.
         for string in identification_strings.keys():
-            if string in self.output:
+            if string in output:
                 identity = identification_strings[string]
                 # Don't add it if it's already there. More than one might match.
-                if identity not in self.identification:
-                    self.identification.append(identity)
+                if identity not in self.identifiers:
+                    self.identifiers.append(identity)
 
         return True
 
-    def _expect_prompt(self):
-        """Expect something that looks like our prompt. It may have changed by
-        now because of a mode switch, so if it did, update the prompt
-        definition."""
-
-        # We currently allow a certain set of common prompt chars.
-        prompt_chars = ['>', '#', ']', ':']
-        general_prompt_regex = '.*%s\s*$'
-        # Create a regex for each char in the list above.
-        prompt_regexes = [ general_prompt_regex % (char) for char in prompt_chars ]
-        # Expect one of the regexes or the prompt.
-        self.interact.expect([self.prompt] + prompt_regexes)
-        # If we didn't match the prompt itself, get a new prompt.
-        if (self.interact.last_match != self.prompt):
-            self._get_prompt()
-
-        return self.prompt
-
-    def send(self, command):
+    def send(self, command, graceful=False):
         """Run a command.
 
         Here's what a "send" does:
@@ -208,18 +277,29 @@ class CLIWrangler:
         # Send the command.
         self.interact.send(command)
 
-        # Expect something that looks like our prompt, although it might have changed by now.
-        self._expect_prompt()
+        # On the off chance that we couldn't disable paging, hit space bar a
+        # few times. This is pretty sad, but necessary for things like the
+        # ASAs, which don't let you disable paging until you've enabled.
+        #self.interact.channel.send('     ')
 
-        # Set the output.
-        self.output = self.interact.current_output_clean
-        self.output_raw = self.interact.current_output
+        # Expect the end of the output of the send command.
+        self._expect_output()
 
-        # If something looked like an error, print it and raise an exception.
-        error_patterns = ['^% Invalid', '^ERROR: ', '^Cannot make changes']
+        # If something looked like an error, print it and maybe raise an exception.
+        error_patterns = ['^% Invalid', '^ERROR: ', '^Cannot make changes', 'Command fail. Return code']
         for error in error_patterns:
             if re.search(error, self.output, flags=re.MULTILINE):
-                raise Exception("Found error string! \n%s" % (self.output))
+                # If they don't want exceptions, don't raise an exception.
+                # The reason why I offer the choice is because some Python
+                # programmers do everything by exception handling, and others
+                # hate it.
+                if graceful:
+                    # We return None if there was an error and the user didn't
+                    # want exceptions to be raised. If the user wants to check
+                    # for a specific error, it will be in self.output.
+                    return None
+                else:
+                    raise Exception("Found error string! \n%s" % (self.output))
 
         # Return the output.
         return self.output
@@ -242,8 +322,9 @@ perform that action.
 
         # Type the password at the password prompt.
         if re.search('ssword:', self.interact.last_match, flags=re.IGNORECASE):
-            self.interact.send(enable_password)
-            self._expect_prompt()
+            self.send(enable_password)
+            #self.interact.send(enable_password)
+            #self._expect_prompt()
         else:
             raise Exception("Didn't get something that looked like a password prompt when trying to enable!")
 
@@ -261,4 +342,67 @@ perform that action.
         self.interact.send(self.newline * 2)
         self.interact.take_control()
         return True
+
+    def safe_to_modify(self):
+        """Run a series of tests to figure out whether we should make config
+        changes on this device.
+
+        Most things should return True, but the secondary (inactive) device in
+        a redundant pair should return False. If we can't figure it out, return 
+        None."""
+
+        # We should use the identifiers to figure out how to proceed.
+        # Cisco ASAs and FWSMs need to be checked to make sure they're
+        # standalone or currently active.
+        if "Cisco" in self.identifiers:
+            if "ASA" in self.identifiers:
+                # Run "show failover" on the ASA and look for various strings.
+                # Has to be graceful because this command requires the failover license.
+                self.send('show failover', graceful=True)
+                if re.search('Command requires failover license', self.output):
+                    return True
+                elif 'Failover Off' in self.output:
+                    return True
+                elif re.search('This host:.*- Active', self.output):
+                    return True
+                elif re.search('This host:.*- Standby', self.output):
+                    return False
+                else:
+                    # We don't know for sure, so return None.
+                    return None
+            elif "FWSM" in self.identifiers:
+                # We run "show failover" on the FWSM and look for various strings.
+                self.send('show failover', graceful=True)
+                if 'Failover Off' in self.output:
+                    return True
+                elif 'This context: Active' in self.output:
+                    return True
+                elif 'This context: Standby' in self.output:
+                    return False
+                elif re.search('This Host:.*- Active', self.output):
+                    return True
+                elif re.search('This Host:.*- Standby', self.output):
+                    return False
+                else:
+                    # We don't know for sure, so return None.
+                    return None
+            else:
+                # We'll always give a green light for Cisco devices.
+                return 1
+        # On Fortinet devices, this probably won't always work, but it works
+        # for FortiOS 5.x so far.
+        elif "Fortinet" in self.identifiers:
+            if "FortiGate" in self.identifiers:
+                self.send('get system status', graceful=True)
+                if 'Current HA mode: a-p, master' in self.output:
+                    return True
+                elif 'Current HA mode: a-p, backup' in self.output:
+                    return False
+                elif 'Current HA mode: standalone' in self.output:
+                    return True
+                else:
+                    # We don't know for sure, so return None.
+                    return None
         
+        # For anything else, we aren't qualified to say with certainty.
+        return None
